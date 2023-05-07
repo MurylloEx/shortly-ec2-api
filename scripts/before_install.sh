79eda39335bcfed6d9c83cde737c810ae43a61c7
MurylloEx <muryllopimenta@gmail.com>
2023-05-07 19:46:19 -0300

    ci: trigger deployment

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,5 +3,8 @@ set -e
 
 echo "[🚀] Running step 'BeforeInstall'..."
 echo "[🚀] Installing dependencies..."
+
+cd /home/ubuntu/code-deploy/shortly-backend
 sudo npm install
+
 echo "[🚀] Installed dependencies..."
